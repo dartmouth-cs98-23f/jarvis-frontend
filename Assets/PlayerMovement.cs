ffe80911328cb3d3caa505eacf052daa24f20e25
Evan Phillips <evanjphillips@gmail.com>
2023-10-29 21:38:59 -0400

    undid changes to playermovement script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 targetPosition;
-    bool canMove = true;
+
     private float moveSpeed = 5.0f;
 
     // Start is called before the first frame update
@@ -19,10 +19,10 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayerByClick()
     {
-        if (Input.touchCount > 0) 
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began) 
+            if (touch.phase == TouchPhase.Began)
             {
                 targetPosition = Camera.main.ScreenToWorldPoint(touch.position);
             }
@@ -33,25 +33,15 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(Mathf.Round(direction.x * moveSpeed), Mathf.Round(direction.y * moveSpeed));
         }
-        else 
+        else
         {
             rb.velocity = Vector2.zero;
         }
     }
 
-    public void OnHamburgerMenuClick()
-    {
-        canMove = false;
-        Debug.Log("Hamburger Menu Clicked!");
-    }
-
-
     // Update is called once per frame
     void Update()
     {
-        if (canMove){
-            MovePlayerByClick();
-        }
-        
+        MovePlayerByClick();
     }
 }
