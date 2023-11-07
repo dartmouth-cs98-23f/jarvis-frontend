259ec63b722abf19956e38f8f86aa73bc81f471c
Vico Lee <vicoleezy@gmail.com>
2023-11-07 13:23:53 -0700

    added ChatMessage structure with SendMessage placeholder

--- Assets/ChatManager.cs
+++ Assets/ChatManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class Chat
+public class ChatMessage
 {
     public string senderId;
     public string receiverId;
@@ -16,6 +16,25 @@ public class ChatManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public string otherUserID;
+    public InputField messageInputField;
+    public string chatTestJsonString = @"
+    {
+        ""1"": {
+            ""senderId"": ""someSenderId"",
+            ""receiverId"": ""someReceiverId"",
+            ""content"": ""someContent"",
+            ""isGroupChat"": false,
+            ""createdTime"": ""someCreatedTime""
+        },
+        ""2"": {
+            ""senderId"": ""someOtherSenderId"",
+            ""receiverId"": ""someOtherReceiverId"",
+            ""content"": ""someOtherContent"",
+            ""isGroupChat"": false,
+            ""createdTime"": ""someOtherCreatedTime""
+        }
+    }";
+
     void Start()
     {
         initializeChatHistory();
@@ -27,10 +46,34 @@ public class ChatManager : MonoBehaviour
         
     }
 
+    // send message to backend
+    void onSendMessage() // TODO: fjaeiowf
+    {
+        ChatMessage message =  new ChatMessage();
+        message.senderId = "placeholderId";
+        message.receiverId = otherUserID;
+        message.content = text;
+        message.isGroupChat = false;
+        message.createdTime = DateTime.Now; // TODO: check if this is auto-generated on backend
+        SendMessage(otherUserID, messageInputField.text);
+    }
+
+    void SendMessage(string otherUserID, string text)
+    {
+
+    }
+
+
     void initializeChatHistory()
     {
         string chatJsonString = GetChatHistory(otherUserID);
 
+    }
+
+    // TODO: replace this with the actual backend call
+    void GetChatHistory(string otherUserID)
+    {
+        return chatTestJsonString;
     }
 
 }
