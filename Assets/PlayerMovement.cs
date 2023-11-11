65ad1401f62d3c220712e0aa4d0deaf2345a9567
Evan Phillips <evanjphillips@gmail.com>
2023-11-11 17:27:31 -0500

    got collisions and retrieving npc id working

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 targetPosition;
     private Animator animator;
     private float moveSpeed = 5.0f;
+    private Guid collidedUserId;
 
     // Start is called before the first frame update
     void Start()
@@ -73,6 +74,37 @@ public class PlayerMovement : MonoBehaviour
     }
 }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+{
+    Debug.Log($"Collided with GameObject: {collision.gameObject.name}, Tag: {collision.gameObject.tag}");
+
+    // Check if the collision is with an NPC
+    if (collision.gameObject.CompareTag("NPC"))
+    {
+        NPCCharacter npcCharacter = collision.gameObject.GetComponent<NPCCharacter>();
+
+        if (npcCharacter != null)
+        {
+            // Access NPC information
+            collidedUserId = npcCharacter.GetUserId();
+
+            Debug.Log($"Collided with NPC (ID: {collidedUserId})");
+
+            // Disable the Rigidbody2D to stop the NPCCharacter from moving
+            Rigidbody2D npcRigidbody = npcCharacter.GetComponent<Rigidbody2D>();
+            if (npcRigidbody != null)
+            {
+                npcRigidbody.bodyType = RigidbodyType2D.Static; // Set to Static to make it immovable
+            }
+
+            // Store the collidedUserId in PlayerPrefs
+            PlayerPrefs.SetString("CollidedUserId", collidedUserId.ToString());
+
+            // Load the "Chat" scene
+            SceneManager.LoadScene("Chat");
+        }
+    }
+}
 
     // Update is called once per frame
     async void Update()
---
+++ Assets/Scripts/NPCCharacter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+
+public class NPCCharacter : MonoBehaviour
+{
+    // User-specific information
+    public Guid userId = new Guid("375d96a5-492d-43a0-af8c-6db76ce341d3");
+
+    // Method to get the user ID
+    public Guid GetUserId()
+    {
+        return userId;
+    }
+}
