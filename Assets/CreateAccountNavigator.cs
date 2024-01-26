42d58e0ae5a788db0bcd4a578a1b05899ca89a38
Vico Lee <vicoleezy@gmail.com>
2024-01-26 11:25:50 -0500

    added BuildYourself panels, characterSummary panels, tutorialPanel, createAccount panel

--- Assets/CreateAccountNavigator.cs
+++ Assets/CreateAccountNavigator.cs
@@ -9,8 +9,7 @@ using UnityEngine.SceneManagement;
 public class CreateAccountNavigator : MonoBehaviour
 {
 
-    public InputField firstNameInput;
-    public InputField lastNameInput;
+    public InputField usernameInput;
     public InputField emailInput;
     public InputField passwordInput;
     public InputField confirmPasswordInput;
@@ -24,15 +23,14 @@ public class CreateAccountNavigator : MonoBehaviour
     }
 
     public async void OnCreateAccountButtonClicked()
-    {
-        string firstName = firstNameInput.text;
-        string lastName = lastNameInput.text;
+    {   
+        string username = usernameInput.text;
         string email = emailInput.text;
         string password = passwordInput.text;
         string confirmPassword = confirmPasswordInput.text;
 
         // TODO: Add an UI error message for users
-        if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
         {
             Debug.Log("Please fill out all fields");
             return;
@@ -46,12 +44,13 @@ public class CreateAccountNavigator : MonoBehaviour
         }
 
         // TODO: Get error message from backend and display it
-        bool registrationSuccessful = await CreateAccount(firstName, lastName, email, password, confirmPassword);
+        // bool registrationSuccessful = true;
+        bool registrationSuccessful = await CreateAccount(username, email, password, confirmPassword);
 
         if (registrationSuccessful)
         {
             NavigateToQuestionPanels();
-            Debug.Log("Successfully created account with first name: " + firstName + ", last name: " + lastName + " email: " + email + " and password: " + password + "");
+            Debug.Log("Successfully created account with username: " + username + " email: " + email + " and password: " + password + "");
         }
         else
         {
@@ -65,17 +64,18 @@ public class CreateAccountNavigator : MonoBehaviour
     //  "error: password must be at least 8 characters"
     // "error: passwords do not match"
     // "error: email is not valid"
-    private async Task<bool> CreateAccount(string firstName, string lastName, string email, string password, string confirmPassword)
-{
-    HTTPClient httpClient = HTTPClient.Instance;
-    bool registrationSuccessful = await httpClient.RegisterUser(firstName, lastName, email, password);
-    if (registrationSuccessful){
-        return true;
+    private async Task<bool> CreateAccount(string username, string email, string password, string confirmPassword)
+    {
+        HTTPClient httpClient = HTTPClient.Instance;
+        bool registrationSuccessful = true;         // comment out this line to connect with backend API.
+        // bool registrationSuccessful = await httpClient.RegisterUser(firstname, lastname, email, password); // TODO: update firstname lastname with username and uncomment
+        if (registrationSuccessful){
+            return true;
+        }
+        else{
+            return false; 
+        }
     }
-    else{
-        return false; 
-    }
-}
 
 
     private void NavigateToQuestionPanels()
---
+++ Assets/Onboarding/CharacterSummaryManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CharacterSummaryManager : MonoBehaviour
+{
+    public Text username;
+    public Image userImage;
+    public Text userSummary;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        username.GetComponent<Text>().text = "@" + "testUsername";
+        userSummary.GetComponent<Text>().text = "You are a compassionate, wise, and brave Jedi Master. You're known for your strategic mind, moral integrity, mentorship skills, patience, and a subtle sense of humor. You are a compassionate, wise, and brave Jedi Master. You're known for your strategic mind, moral integrity, mentorship skills, patience, and a subtle sense of humor.";
+        userImage.sprite = Resources.Load<Sprite>("Shapes/obi_wan_kenobi");
+    }
+}
