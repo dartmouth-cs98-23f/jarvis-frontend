08456966465c654a04c1daf2e82e1489ab77fc10
Vico Lee <vicoleezy@gmail.com>
2024-02-20 17:04:37 -0500

    added player movement and other player movement. added a testOtherplayermovementcontroller script

---
+++ Assets/MainMap/PlayerMovement.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Clients;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+using System.Threading.Tasks;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 targetPosition;
+    private Animator animator;
+    private float moveSpeed = 5.0f;
+    private Guid collidedCharacterId;
+
+    public GameObject InteractButton;
+    public Tilemap tilemap; // Reference to the Tilemap
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetTilemap(Tilemap tilemap)
+    {
+        Debug.Log("Setting tilemap");
+        this.tilemap = tilemap;
+    }
+    
+    private async Task MovePlayerByClick()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Clicked on a UI element, do not move the player
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            {
+                return;
+            }
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                targetPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            }
+        }
+
+        // This fixes the player automatically going to 0,0 on start
+        if (targetPosition.x == 0f && targetPosition.y == 0f)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
+
+        // Calculate the absolute values of movement in x and y directions
+        float absMoveX = Mathf.Abs(direction.x);
+        float absMoveY = Mathf.Abs(direction.y);
+
+        // Determine which animation to play based on the movement direction
+        if (absMoveY > absMoveX)
+        {
+            // Play walk forward animation
+            animator.SetFloat("MoveX", 0f);
+            animator.SetFloat("MoveY", direction.y);
+        }
+        else
+        {
+            // Play walk right animation
+            animator.SetFloat("MoveX", direction.x);
+            animator.SetFloat("MoveY", 0f);
+        }
+
+        // Check if the distance between the player and the target is greater than the stopping distance
+        if (Vector2.Distance(targetPosition, transform.position) > 0.1f)
+        {
+            // Calculate the next position
+            Vector2 nextPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+
+            // Check if the next position is within the bounds of the tilemap
+            if (IsWithinTilemapBounds(nextPosition))
+            {
+                // Move the player towards the target position
+                rb.velocity = new Vector2(Mathf.Round(direction.x * moveSpeed), Mathf.Round(direction.y * moveSpeed));
+            }
+            else
+            {
+                // Stop the player if the next position is outside the tilemap bounds
+                rb.velocity = Vector2.zero;
+            }
+
+            // Get the current player position
+            int xCoordinate = Mathf.RoundToInt(transform.position.x);
+            int yCoordinate = Mathf.RoundToInt(transform.position.y);
+
+            // TODO: Comment out when backend works
+            PlayerPrefs.SetString("coordX", xCoordinate.ToString());
+            PlayerPrefs.SetString("coordY", yCoordinate.ToString()); 
+
+            // TODO: Backend api connection. Send the updated location to the server
+            // await SignalRClient.Instance.UpdateLocation(xCoordinate, yCoordinate);
+        }
+        else
+        {
+            // If the player is within the stopping distance, stop its movement and animation
+            rb.velocity = Vector2.zero;
+            animator.SetFloat("MoveX", 0.001f);
+            animator.SetFloat("MoveY", 0.001f);
+        }
+    }
+
+    private bool IsWithinTilemapBounds(Vector2 position)
+    {
+        // Convert world position to tile position
+        Vector3Int cellPosition = tilemap.WorldToCell(position);
+
+        // Check if the cell position is within the bounds of the tilemap
+        return tilemap.cellBounds.Contains(cellPosition);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"Collided with GameObject: {collision.gameObject.name}, Tag: {collision.gameObject.tag}");
+
+        // Show click to chat button
+        InteractButton.SetActive(true);
+
+        if (collision.gameObject.CompareTag("user") || collision.gameObject.CompareTag("agent"))
+        {
+            CharacterComponent characterComponent = collision.gameObject.GetComponent<CharacterComponent>();
+
+            if (characterComponent != null)
+            {
+                // Access NPC information
+                collidedCharacterId = characterComponent.GetCharacterId();
+                string collidedCharacterType = characterComponent.GetCharacterType();
+
+                Debug.Log($"Collided with Character Type: {collidedCharacterType} (ID: {collidedCharacterId})");
+
+                // Store the CollidedCharacterId in PlayerPrefs to access in the Chat scene
+                // TODO: Do this without using PlayerPrefs
+                PlayerPrefs.SetString("CollidedCharacterId", collidedCharacterId.ToString());
+                PlayerPrefs.SetString("CollidedCharacterType", collidedCharacterType);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // Hide click to chat button
+        InteractButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {
+        await MovePlayerByClick();
+    }
+}
+
--- Assets/Scripts/GameClient.cs
+++ Assets/Scripts/GameClient.cs
@@ -1,29 +1,46 @@
 using UnityEngine;
 using Clients;
 using System;
+using System.Collections.Generic;
 using UnityEngine.Tilemaps;
 
+
+public class CharacterType
+{
+    public static string User = "user";
+    public static string Agent = "agent";
+}
 
 public class GameClient : MonoBehaviour
 {
 
     private Guid userId;
-    public GameObject currentPlayerPrefab;
+    private Guid worldId;
+    public GameObject currentUserPrefab;
+    public GameObject otherUserPrefab;
+    public GameObject AgentPrefab;
+    public GameObject EggPrefab;
     public GameObject georgePrefab;
     public GameObject yodaPrefab;
-    // public GameObject NPCPlayerPrefab;
-    public SignalRClient signalRClient;
     public Transform mainMap;
 
     private HTTPClient httpClient;
     private HTTPClient.UserData currentUserData;
-
+    private List<HTTPClient.UserData> allUsers = new List<HTTPClient.UserData>();
+    private List<HTTPClient.AgentData> allAgents = new List<HTTPClient.AgentData>();
+    private HashSet<Guid> characterIdSet = new HashSet<Guid>();
     void Awake()
     {
         httpClient = HTTPClient.Instance; // get httpClient
-        userId = httpClient.MyId;
-        InitializeGame();
+        // TODO: Uncomment below for backend connection
+        // userId = httpClient.MyId;
+        // worldId = httpClient.CurrentWorldId();
 
+        // TODO: Comment below out for backend connection
+        userId = new Guid("c0c973f7-5f80-437e-8418-f3c401780274");
+        worldId = new Guid("3b490737-6d3f-4bb8-9593-15e8a1c80dab");
+
+        InitializeGame();
     }
 
     void Update()
@@ -37,11 +54,8 @@ public class GameClient : MonoBehaviour
         if (currentUserData != null)
         {
             Debug.Log("InitializeGame: " + currentUserData.username);
-            Debug.Log("InitializeGame: " + currentUserData.location.coordX + ", " + currentUserData.location.coordY);
-            BuildAllCharacters(); // Call this method after user data is initialized
-            await SignalRClient.Initialize(httpClient.AuthToken, currentUserData.username); // TODO: Change first name to user first name later
-            signalRClient = SignalRClient.Instance;
-            // signalRClient.RegisterUpdateLocationHandler(); // register updateLocation handler
+            // BuildAllCharacters(); // Call this method after user data is initialized
+            BuildAllCharactersV2(); // Call this method after user data is initialized
         }
         else
         {
@@ -49,9 +63,121 @@ public class GameClient : MonoBehaviour
         }
     }
 
+    void BuildAllCharactersV2()
+    {
+        BuildAllUsers();
+        BuildAllAgents();
+    }
+
+    async void BuildAllAgents()
+    {
+        allAgents = await httpClient.GetWorldAgents(worldId);
+        foreach (HTTPClient.AgentData agent in allAgents)
+        {
+            if (characterIdSet != null && characterIdSet.Contains(agent.id))
+            {
+                Debug.Log("Agent with id: " + agent.id + " already exists. Skipping...");
+                continue;
+            } else {
+                characterIdSet.Add(agent.id);
+            }
+            GameObject agentPrefab = GenerateAgentPrefab(agent);
+            GameObject agentGO = Instantiate(agentPrefab, mainMap); // TODO: Replace georgePrefab with actual user prefab
+            agentGO.tag = CharacterType.Agent;
+
+            CharacterComponent agentComponent = agentGO.GetComponent<CharacterComponent>();
+            agentComponent.SetPosition(agent.location.coordX, agent.location.coordY, 0);
+            agentComponent.SetCharacterId(agent.id);
+            agentComponent.SetCharacterType(CharacterType.Agent);
+
+            DisableCharacterRigidBody(agentComponent);
+        }
+    }
+
+    // Disable the Rigidbody2D to stop character from moving due to collisions
+    void DisableCharacterRigidBody(CharacterComponent characterComponent)
+    {
+        Rigidbody2D characterRigidbody = characterComponent.GetComponent<Rigidbody2D>();
+        if (characterRigidbody != null)
+        {
+            characterRigidbody.bodyType = RigidbodyType2D.Static; 
+        }
+    }
+
+    GameObject GenerateAgentPrefab(HTTPClient.AgentData agent)
+    {
+        if (agent.isHatched) {
+            return AgentPrefab; // TODO: Add logic to get actual agent prefab
+        } else {
+            return EggPrefab; // TODO: Add logic for this to display the incubation time and to interact with it - nurture it
+        }
+    }
+    
+
+    async void BuildAllUsers()
+    {
+        allUsers = await httpClient.GetWorldUsers(worldId);
+        foreach (HTTPClient.UserData user in allUsers)
+        {
+            Debug.Log("Building user: " + user.username + " with id: " + user.id);
+            if (characterIdSet != null && characterIdSet.Contains(user.id))
+            {
+                Debug.Log("User with id: " + user.id + " already exists. Skipping...");
+                continue;
+            } else {
+                characterIdSet.Add(user.id);
+            }
+
+            GameObject userPrefab = GenerateUserPrefab(user.id);
+            GameObject userGO = Instantiate(userPrefab, mainMap); // TODO: Replace georgePrefab with actual user prefab
+            userGO.tag = CharacterType.User;
+
+            if (user.id == userId)
+            {
+                PlayerMovement userMovementScript = userGO.GetComponent<PlayerMovement>();
+                userMovementScript.InteractButton = GameObject.Find("ChatButton");
+                userMovementScript.InteractButton.SetActive(false);
+                userMovementScript.SetTilemap(GameObject.Find("Tilemap").GetComponent<Tilemap>());
+            } else {
+                OtherPlayerMovement otherUserMovementScript = userGO.GetComponent<OtherPlayerMovement>();
+            }
+
+            CharacterComponent userComponent = userGO.GetComponent<CharacterComponent>();
+            userComponent.SetCharacterId(user.id);
+            userComponent.SetCharacterType(CharacterType.User);
+            
+            if (user.location == null)
+            {
+                Debug.Log("User location is null. Setting coordinates to 0, 0");
+                userComponent.SetPosition(0, 0, 0);
+                continue;
+            } else {
+                userComponent.SetPosition(user.location.coordX, user.location.coordY, 0);
+            }
+            
+            // Disable the Rigidbody2D to stop character from moving due to collisions
+            DisableCharacterRigidBody(userComponent);
+        }
+    }
+
+    // TODO: Update this method to generate the actual prefab of the user
+    GameObject GenerateUserPrefab(Guid userId)
+    {
+        if (userId == this.userId)
+        {
+            Debug.Log("Generating main user prefab with userId: " + userId);
+            return currentUserPrefab;   // this prefab would have the playerMovement script attached
+        }
+        else
+        {
+            Debug.Log("Generating other user prefab with userId: " + userId);
+            return otherUserPrefab; // this prefab would have the otherPlayerMovement script attached
+        }
+    }
+
     // c8268729-e4f5-4df3-85fc-51779d7c2b35
     //d2e12e14-5df1-4a0e-92f1-11f2f10e5880
-
+    // @Deprecated: This is from V1 (MVP)
     void BuildAllCharacters()
     {
         // TODO: Uncomment below for local testing
@@ -72,7 +198,7 @@ public class GameClient : MonoBehaviour
             return;
         }
 
-        GameObject currentUserGO = Instantiate(currentPlayerPrefab, mainMap);
+        GameObject currentUserGO = Instantiate(currentUserPrefab, mainMap);
         currentUserGO.tag = "Player";
 
         Tilemap tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
@@ -102,7 +228,7 @@ public class GameClient : MonoBehaviour
         
         CharacterComponent georgeWashingtonComponent = georgeWashingtonGO.GetComponent<CharacterComponent>();
         georgeWashingtonComponent.SetPosition(-10, 10, 0); // Add back httpClient.currentUserData if testing locally
-        georgeWashingtonComponent.SetUserId(new Guid("55cd50d5-7775-4dd2-b632-a502a031ac41")); // Add back httpClient.currentUserData if testing locally
+        georgeWashingtonComponent.SetCharacterId(new Guid("55cd50d5-7775-4dd2-b632-a502a031ac41")); // Add back httpClient.currentUserData if testing locally
 
 
 
@@ -122,7 +248,7 @@ public class GameClient : MonoBehaviour
         
         CharacterComponent yodaComponent = yodaGO.GetComponent<CharacterComponent>();
         yodaComponent.SetPosition(10, -3, 0); // Add back httpClient.currentUserData if testing locally
-        yodaComponent.SetUserId(new Guid("f7dd290b-faab-4c15-b8b9-38cff0895559")); // Add back httpClient.currentUserData if testing locally
+        yodaComponent.SetCharacterId(new Guid("f7dd290b-faab-4c15-b8b9-38cff0895559")); // Add back httpClient.currentUserData if testing locally
 
     }
     
